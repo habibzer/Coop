0f66da7812bc7afed640b412cedda2b0501df7e3
Habib Benharrat <benharrat.habib1@gmail.com>
2020-02-07 10:01:28 +0100

    detail membre fini

--- src/components/DetailMembre.vue
+++ src/components/DetailMembre.vue
@@ -2,8 +2,14 @@
 
     <div>
 
-        <h1>Detail {{membre}} !</h1>
+        <h1>Profile de : {{membre.fullname}} !</h1>
+            <h2 class="info">informations :</h2>
+                <ul>
+                    <li class="details">Nom : {{membre.fullname}}</li>
+                    <li class="details">Adresse mail : {{membre.email}}</li>
+                    <li class="details">Membre depuis : {{membre.created_at}}</li>
 
+                </ul>
     </div>
 
 </template>
@@ -12,24 +18,31 @@
     export default {
         name : 'DetailMembre',
 
-
         data:function () {
             return {
-
                 membre : false
-
             }
-
         },
         mounted()  {
             this.membre = this.getMembre(this.$route.params.identifiant);
-            alert(this.$route.params.identifiant)
+            /*alert(this.$route.params.identifiant)*/
         },
         methods:{
+
 
         }
     }
 </script>
 <style>
 
+    .details{
+        text-align: left;
+        margin-left: 600px;
+        margin-top: 10px;
+    }
+    .info{
+        margin-top: 20px;
+        margin-bottom: 60px;
+
+    }
 </style>
--- src/outils.js
+++ src/outils.js
@@ -1,14 +1,15 @@
 export const outils = {
     methods : {
         getMembre(id) {
-            this.$store.state.membres
-            /* TO DO...
             for(var i= 0; i < this.$store.state.membres.length; i++)
             {
-                if(id == this.$store.state.membres.id){
-                    alert('ok')
+                let membre = this.$store.state.membres[i];
+                if(id == membre.id){
+                    return membre;
+                  /*  console.log(membre)*/
                 }
-            }*/
+            }
+
         }
     }
 }
